2df8328142c47a47a06d29d2b8482b1fbf497896
michaeljlin <mjameslin@gmail.com>
2018-03-05 12:32:01 -0800

    Started lobby & creating rooms

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Chat from './chat';
+import Lobby from './lobby';
 
 const App = () => (
     <div>
         <div className="container">
             <h1 className="center-align">Firechat 🔥</h1>
-            <Chat />
+            <Lobby />
         </div>
     </div>
 );
