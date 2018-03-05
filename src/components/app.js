a02a15bcdd175ee8577bc45a9895a61479ad2b6f
michaeljlin <mjameslin@gmail.com>
2018-03-05 12:13:49 -0800

    Firebase working

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import Chat from './chat';
 
 const App = () => (
     <div>
         <div className="container">
-            <h1 className="center-align">Firechat</h1>
+            <h1 className="center-align">Firechat 🔥</h1>
+            <Chat />
         </div>
     </div>
 );
