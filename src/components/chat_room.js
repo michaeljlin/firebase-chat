3d00f34335fc49d0d773278e1f34fd62101bcf51
michaeljlin <mjameslin@gmail.com>
2018-03-05 17:05:42 -0800

    Established chat log snapshot

--- src/components/chat_room.js
+++ src/components/chat_room.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getRoomData } from '../actions';
+import { db } from '../firebase';
 
 class ChatRoom extends Component{
     componentDidMount(){
         const { roomId, logId } = this.props.match.params;
         this.props.getRoomData(roomId, logId);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(!this.props.roomInfo.chatLogId && nextProps.roomInfo.chatLogId){
+            console.log('We now have chat log ID');
+            db.ref(`/chat-logs/${nextProps.roomInfo.chatLogId}`).on('value', (snapshot)=>{
+                console.log('Chat log snapshot: ', snapshot.val());
+            });
+        }
     }
 
     render(){
